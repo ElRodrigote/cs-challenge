03b793267d067cacf9a08c14a9752fe57cb7d7b1
Rorry <rodrigojali@gmail.com>
2023-07-12 13:45:30 -0300

    styles: force light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Chainsafe Challenge",
-  description: "Public Holidays Calendar for Chainsafe Frontend challenge",
+  title: 'Chainsafe Challenge',
+  description: 'Public Holidays Calendar for Chainsafe Frontend challenge',
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark:bg-white">
       <body className={inter.className} suppressHydrationWarning>
         {children}
       </body>
